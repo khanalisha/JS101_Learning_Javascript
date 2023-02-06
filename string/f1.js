67cb1167ebfe54b0ca12c28133918b23b2437646
Alisha <8282alishakhan@gmail.com>
2023-02-06 19:33:13 +0530

    string

---
+++ string/f1.js
@@ -0,0 +1,8 @@
+let arr='alisha sana goku';
+let space=0;
+for(i=0;i<arr.length;i++){
+  if(arr[i]==" "){
+    space++
+  }
+}
+console.log(space+1);
---
+++ string/f2.js
@@ -0,0 +1,6 @@
+let arr=["golu","goku",];
+let character=0;
+for(i=0;i<arr.length;i++){
+  character=character+arr[i].length
+}
+console.log(character);
